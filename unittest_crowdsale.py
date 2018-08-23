eb3e94d1fb9a139524ef8f0cfe59b17e2a82f377
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-08-23 15:40:40 +0300

    reorganize test. initialize before each test

--- unittest_crowdsale.py
+++ unittest_crowdsale.py
@@ -14,13 +14,8 @@ setup.use_keosd(False)
 
 
 class CrowdsaleTests(unittest.TestCase):
-    def run(self, result=None):
-        """ Stop after first error """
-        if not result.failures:
-            super().run(result)
-
     @classmethod
-    def setUpClass(cls):
+    def parseConfig(cls):
         global cfg
         cfg = {}
         with open('config.h', 'r') as cfg_file:
@@ -29,86 +24,125 @@ class CrowdsaleTests(unittest.TestCase):
                 if match:
                     cfg[match.group(1)] = match.group(2)
 
+    @classmethod
+    def setUpClass(cls):
+        cls.parseConfig()
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def run(self, result=None):
+        """ Stop after first error """
+        if not result.failures:
+            super().run(result)
+
+    def setUp(self):
+        # start node
         assert (not node.reset().error)
-        global wallet
-        global eosio
-        global mywishtoken5
-        global token_deployer
-        global crowdsale_deployer
 
-        wallet = eosf.Wallet()
+        # create wallet
+        self.wallet = eosf.Wallet()
 
-        eosio = eosf.AccountMaster()
-        wallet.import_key(eosio)
+        # create eosio account
+        self.eosio_acc = eosf.AccountMaster()
+        self.wallet.import_key(self.eosio_acc)
 
+        # create MINTDEST accounts
         for x in range(int(cfg["MINTCNT"])):
-            wallet.import_key(eosf.account(eosio, cfg["MINTDEST" + str(x)]))
+            self.wallet.import_key(eosf.account(self.eosio_acc, cfg["MINTDEST" + str(x)]))
 
-        token_deployer = eosf.account(eosio, cfg["CONTRACT"])
-        wallet.import_key(token_deployer)
+        # create system token deployer account
+        self.system_token_deployer_acc = eosf.account(self.eosio_acc, "eosio.token")
+        self.wallet.import_key(self.system_token_deployer_acc)
 
-        crowdsale_deployer = eosf.account(eosio, "ico.deployer")
-        wallet.import_key(crowdsale_deployer)
+        # create token deployer account
+        self.token_deployer_acc = eosf.account(self.eosio_acc, cfg["CONTRACT"])
+        self.wallet.import_key(self.token_deployer_acc)
 
-        global contract_eosio_bios
-        contract_eosio_bios = eosf.Contract(eosio, "eosio.bios")
-        assert (not contract_eosio_bios.error)
-        deployment_bios = contract_eosio_bios.deploy()
+        # create crowdsale deployer account
+        self.crowdsale_deployer_acc = eosf.account(self.eosio_acc, "ico.deployer")
+        self.wallet.import_key(self.crowdsale_deployer_acc)
+
+        # deploy eosio.bios contract
+        self.eosio_bios_contract = eosf.Contract(self.eosio_acc, "eosio.bios")
+        assert (not self.eosio_bios_contract.error)
+        deployment_bios = self.eosio_bios_contract.deploy()
         assert (not deployment_bios.error)
 
-        global contract_token
-        contract_token = eosf.Contract(
-            token_deployer,
+        # deploy system token contract
+        self.system_token_contract = eosf.Contract(self.system_token_deployer_acc, "eosio.token")
+        assert (not self.system_token_contract.error)
+        deployment_system_token_contract = self.system_token_contract.deploy()
+        assert (not deployment_system_token_contract.error)
+        assert (not self.system_token_deployer_acc.code().error)
+
+        # deploy custom eosio.token contract
+        self.token_contract = eosf.Contract(
+            self.token_deployer_acc,
             "eosio-crowdsale/eosiotoken/eosio.token",
             wast_file='eosio.token.wast',
             abi_file='eosio.token.abi'
         )
-        assert (not contract_token.error)
-        deployment_token = contract_token.deploy()
+        assert (not self.token_contract.error)
+        deployment_token = self.token_contract.deploy()
         assert (not deployment_token.error)
-        assert (not token_deployer.code().error)
+        assert (not self.token_deployer_acc.code().error)
 
-        assert (not contract_token.push_action(
+        # create system eos asset
+        assert (not self.system_token_contract.push_action(
             "create",
             json.dumps({
-                "issuer": str(crowdsale_deployer),
-                "maximum_supply": "10000019375.00 WISH"
+                "issuer": str(self.system_token_deployer_acc),
+                "maximum_supply": "1009410066.0000 EOS"
             }),
-            token_deployer
+            self.system_token_deployer_acc
         ).error)
 
-        global contract_crowdsale
-        contract_crowdsale = eosf.Contract(
-            crowdsale_deployer,
+        # create custom token asset
+        assert (not self.token_contract.push_action(
+            "create",
+            json.dumps({
+                "issuer": str(self.crowdsale_deployer_acc),
+                "maximum_supply": "10000019375.00 WISH"
+            }),
+            self.token_deployer_acc
+        ).error)
+
+        # deploy crowdsale contract
+        self.crowdsale_contract = eosf.Contract(
+            self.crowdsale_deployer_acc,
             "eosio-crowdsale/crowdsale",
             wast_file='crowdsale.wast',
             abi_file='crowdsale.abi'
         )
-
-        assert (not contract_crowdsale.error)
-
-        deployment_crowdsale = contract_crowdsale.deploy()
+        assert (not self.crowdsale_contract.error)
+        deployment_crowdsale = self.crowdsale_contract.deploy()
         assert (not deployment_crowdsale.error)
-        assert (not crowdsale_deployer.code().error)
+        assert (not self.crowdsale_deployer_acc.code().error)
 
-        assert (not contract_eosio_bios.push_action(
+        # set eosio.code permission
+        self.addEosioCodePermission(self.crowdsale_deployer_acc)
+
+    def addEosioCodePermission(self, account):
+        assert (not self.eosio_bios_contract.push_action(
             "updateauth",
             json.dumps({
-                "account": str(crowdsale_deployer),
+                "account": str(account),
                 "permission": "active",
                 "parent": "owner",
                 "auth": {
                     "threshold": 1,
                     "keys": [
                         {
-                            "key": str(crowdsale_deployer.json["permissions"][1]["required_auth"]["keys"][0]["key"]),
+                            "key": str(account.json["permissions"][1]["required_auth"]["keys"][0]["key"]),
                             "weight": 1
                         }
                     ],
                     "accounts": [
                         {
                             "permission": {
-                                "actor": str(crowdsale_deployer),
+                                "actor": str(account),
                                 "permission": "eosio.code"
                             },
                             "weight": 1
@@ -117,32 +151,26 @@ class CrowdsaleTests(unittest.TestCase):
                     "waits": []
                 }
             }),
-            crowdsale_deployer
+            account
         ).error)
-
-        assert (not contract_crowdsale.push_action(
-            "init",
-            json.dumps({}),
-            crowdsale_deployer,
-            output=True
-        ).error)
-
-    @classmethod
-    def tearDownClass(cls):
-        node.stop()
-
-    def setUp(self):
-        pass
 
     def tearDown(self):
-        pass
+        node.stop()
 
     def test_01(self):
         print("1. Check premint")
+
+        assert (not self.crowdsale_contract.push_action(
+            "init",
+            json.dumps({}),
+            self.crowdsale_deployer_acc,
+            output=True
+        ).error)
+
         for x in range(int(cfg["MINTCNT"])):
             expected_value = int(cfg["MINTVAL" + str(x)]) / 10 ** int(cfg["DECIMALS"])
-            real_value_with_symbol = contract_token\
-                .table("accounts", cfg["MINTDEST" + str(x)])\
+            real_value_with_symbol = self.token_contract \
+                .table("accounts", cfg["MINTDEST" + str(x)]) \
                 .json["rows"][0]["balance"]
             real_value = float(real_value_with_symbol[:-(len(cfg['SYMBOL']) + 1)])
             assert (expected_value == real_value)
